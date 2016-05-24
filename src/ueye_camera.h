fe369135b52707aa9c0efe7e8bb5b12a5d7e868e
francois <administrador@CU-FCHATAIGNER>
2016-05-24 11:11:35 +0200

    created ueye_driver

---
+++ src/ueye_camera.h
@@ -0,0 +1,117 @@
+#ifndef UEYE_DRIVER_CAMERA_H
+#define UEYE_DRIVER_CAMERA_H
+
+#include <vector>
+#include <memory>
+#include <stdexcept>
+
+#include <ros/time.h>
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+
+// Driver for IDS uEye camera
+// @see https://en.ids-imaging.com/manuals/uEye_SDK/EN/uEye_Manual/index.html
+
+namespace ueye
+{
+
+class Camera; // forward declaration
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @class CameraFrame
+ * Holds a camera frame and the corresponding sensor_msgs::Image object
+ */
+ 
+class CameraFrame
+{
+    Camera& camera_;
+    int buffer_id_;
+    sensor_msgs::Image image_;
+    
+public:
+    
+    CameraFrame( Camera& camera, int cols, int rows, const std::string& encoding );
+    virtual ~CameraFrame();
+
+    ros::Time get_timestamp() const;
+    void update_timestamp() { image_.header.stamp = get_timestamp(); }
+    const sensor_msgs::Image& get_image() const { return image_; }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+enum RunMode { RunMode_FreeRun, 
+               RunMode_ExternalTrigger };
+
+enum BinningMode { BinningMode_Off=0, 
+                    BinningMode_2X,
+                    BinningMode_4X,
+                    BinningMode_8X,
+                    BinningMode_16X,
+                    BinningMode_End };
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @class Camera
+ * uEye camera object
+ */
+
+class Camera
+{
+    friend class CameraFrame;
+
+    int handle_;
+    std::shared_ptr<CameraFrame> frame_;
+    sensor_msgs::CameraInfo info_;
+
+protected:
+
+    uint16_t get_binning_rate() const;
+    uint32_t get_buffer_pitch() const;
+    float get_sensor_scaling() const;
+    
+public:
+
+    /**
+     * Constructor
+     * @param[in] camera_id ID in [1-254] of camera to connect to (0: first available camera)
+     * @param[in] aoi_width width in pixels of the requested Area Of Interest (AOI)
+     * @param[in] aoi_height height in pixels of the requested Area Of Interest (AOI)
+     * @param[in] encoding ROS color encoding (see sensor_msgs/image_encodings.h)
+     * @param[in] frame_rate desired frame rate
+     * @param[in] binning_mode level of binning (to reduce image size without scaling down AOI)
+     * @throws std::exception if error occurs
+     */
+    Camera( int camera_id, uint16_t aoi_width, uint16_t aoi_height, 
+                const std::string& encoding, float frame_rate, BinningMode binning_mode );
+                
+    virtual ~Camera();
+    
+    void start_capture( RunMode mode );
+    bool is_capturing() const;
+    bool set_gain( uint8_t master_gain );
+    
+    const sensor_msgs::CameraInfo& get_info() const { return info_; }
+    
+    /**
+     * Poll next frame
+     * @param[in] timeout_ms time in milliseconds to wait for a frame event
+     * @returns pointer to the new frame or NULL if no frame was retrieved
+     * @throws std::exception if error occurs
+     */
+    const CameraFrame* get_frame( int timeout_ms );
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace ueye
+
+#endif // UEYE_DRIVER_CAMERA_H
---
+++ src/ueye_driver.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+
+#include "ueye_camera.h"
+
+#include <image_transport/image_transport.h>
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+int main( int argc, char** argv )
+{
+    ros::init( argc, argv, ROS_PACKAGE_NAME );
+    ros::NodeHandle nh, local_nh("~");
+
+    float frame_rate, publish_rate;
+	int hardware_gain, binning, timeout_ms, image_width, image_height;
+	std::string camera_name, camera_topic, frame_id, color_mode;
+		
+	local_nh.param<int>( "hardware_gain", hardware_gain, 0 );
+	local_nh.param<int>( "binning", binning, ueye::BinningMode_Off );
+	local_nh.param<int>( "timeout_ms", timeout_ms, 100 );
+	local_nh.param<int>( "image_width", image_width, 1600 );
+	local_nh.param<int>( "image_height", image_height, 1200 );
+	local_nh.param<float>( "frame_rate", frame_rate, 30 );
+	local_nh.param<float>( "publish_rate", publish_rate, frame_rate );
+	local_nh.param<std::string>( "camera_name", camera_name, "camera" );
+	local_nh.param<std::string>( "camera_topic", camera_topic, "image_raw" );
+	local_nh.param<std::string>( "frame_id", frame_id, "/camera" );
+	local_nh.param<std::string>( "color_mode", color_mode, "rgb8" );
+
+	if ( binning < 0 || binning >= ueye::BinningMode_End )
+		binning = ueye::BinningMode_Off;
+		
+    image_transport::ImageTransport it(nh);
+	image_transport::CameraPublisher pub = it.advertiseCamera( camera_name + "/" + camera_topic, 1 );
+
+	ros::Rate spinner(publish_rate);
+
+	ueye::Camera camera( 0, image_width, image_height, color_mode, frame_rate, (ueye::BinningMode)binning );
+    camera.set_gain( hardware_gain );
+
+	while( ros::ok() )
+	{
+		const ueye::CameraFrame* frame = camera.get_frame(timeout_ms);		
+		if (frame) pub.publish( frame->get_image(), camera.get_info() );
+        
+	    ros::spinOnce();
+		spinner.sleep();
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
